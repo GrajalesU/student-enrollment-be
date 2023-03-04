110c6c74aef9efe738f2d4f68305b89908c82064
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-03-03 21:53:36 -0500

    Fix environment port config name

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const {
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.DB_NAME || 5000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
