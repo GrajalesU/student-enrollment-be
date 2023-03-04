c2fa35233d953ec59fd42b847f67ababa053e5c7
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-03-03 21:33:21 -0500

    Add deb config

---
+++ config/db.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+});
+
+module.exports = sequelize;
